901200f16beab4d2ed0064308a1f24723864b4a1
charliny <jessicacharliny@gmail.com>
2022-01-03 12:15:10 -0300

    construct arquitecture

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    MenuComponent
+  ],
+  exports: [
+    HeaderComponent,
+    MenuComponent
+  ]
+})
+export class SharedModule { }
